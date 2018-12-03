df6ec4307292819d0fa985e6e797a9e0f03790b1
Kelell <Kelell@192.168.0.10>
2018-12-03 20:12:52 +0000

    Got a basic login ui

--- ArcProof/src/gui/frame.java
+++ ArcProof/src/gui/frame.java
@@ -7,44 +7,34 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-public class frame {
-	frame(){    
-		JFrame f=new JFrame("Button Example"); 
-					//submit button
-		JButton b=new JButton("Submit");    
-		b.setBounds(100,100,140, 40);    
-					//enter name label
-		JLabel label = new JLabel();		
-		label.setText("Enter Name :");
-		label.setBounds(10, 10, 100, 100);
-					//empty label which will show event after button clicked
-		JLabel label1 = new JLabel();
-		label1.setBounds(10, 110, 200, 100);
-					//textfield to enter name
-		JTextField textfield= new JTextField();
-		textfield.setBounds(110, 50, 130, 30);
-					//add to frame
-		f.add(label1);
-		f.add(textfield);
-		f.add(label);
-		f.add(b);    
-		f.setSize(360,640);    
-		f.setLayout(null);    
-		f.setVisible(true);    
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
-		
-							//action listener
-		b.addActionListener(new ActionListener() {
-	        
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-					label1.setText("Name has been submitted.");				
-			}          
-	      });
-		}         
-	
-	
-		public static void main(String[] args) {    
-		    new frame();    
-		}    
- }
+import javax.swing.*;    
+import java.awt.event.*;  
+public class frame {  
+    public static void main(String[] args) {    
+    JFrame f=new JFrame("Login Page");    
+     final JLabel label = new JLabel();            
+     label.setBounds(110,300,250,40);  
+     final JPasswordField value = new JPasswordField();   
+     value.setBounds(110, 200, 200, 30);     
+     JLabel user=new JLabel("Username:");    
+     user.setBounds(10, 110, 110, 100);    
+        JLabel pword=new JLabel("Password:");    
+        pword.setBounds(10, 165, 110, 100);   
+        JButton b = new JButton("Login");  
+        b.setBounds(110,250,140,40);   
+        final JTextField text = new JTextField();  
+        text.setBounds(110, 150, 200, 30);     
+                f.add(value); f.add(user); f.add(label); f.add(pword); f.add(b); f.add(text);  
+                f.setSize(360,640);    
+                f.setLayout(null);    
+                f.setVisible(true);     
+                b.addActionListener(new ActionListener() {  
+                public void actionPerformed(ActionEvent e) {       
+                   String data = "Username: " + text.getText();  
+                   data += ", Password: "   
+                   + new String(value.getPassword());   
+                   label.setText(data);          
+                }  
+             });   
+}  
+}  
